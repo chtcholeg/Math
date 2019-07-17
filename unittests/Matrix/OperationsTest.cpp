a045a21c615d6729d8d95a69982c0ce7d54a73dd
Oleg Shchepilov <chtcholeg@mail.ru>
2019-07-17 09:05:28 +0300

    Unittests: Operations _2

--- unittests/Matrix/OperationsTest.cpp
+++ unittests/Matrix/OperationsTest.cpp
@@ -68,3 +68,43 @@ TEST_F(OperationsTest, Addition)
    auto func1plus2 = [func1, func2](size_t row, size_t column)->double { return func1(row, column) + func2(row, column); };
    CheckForEachElement<double>(*matrix_4x4_1plus2.Matrix_, func1plus2, false, 1.0);
 }
+
+TEST_F(OperationsTest, Multiplication)
+{
+   auto func1 = [](size_t row, size_t column)->double
+   {
+      return static_cast<double>(row) * static_cast<double>(column) + 1;
+   };
+   const auto matrix_4x4_1 = CreateStandardMatrix(4, 4, func1);
+   auto func2 = [](size_t row, size_t column)->double
+   {
+      return static_cast<double>(row)+static_cast<double>(column)+1;
+   };
+   const auto matrix_4x4_2 = CreateStandardMatrix(4, 4, func2);
+   auto func3 = [](size_t row, size_t column)->double
+   {
+      return static_cast<double>(row) / (static_cast<double>(column)+1.0);
+   };
+   const auto matrix_5x5_3 = CreateStandardMatrix(5, 5, func3);
+
+   SMT::OperationResultCode code;
+   std::string description;
+   SMT::CheckIfCanMultiplyTogether<double>(*matrix_4x4_1, *matrix_4x4_2, code, description);
+   EXPECT_EQ(code, SMT::OperationResultCode::Ok);
+
+   SMT::CheckIfCanMultiplyTogether<double>(*matrix_4x4_1, *matrix_5x5_3, code, description);
+   EXPECT_EQ(code, SMT::OperationResultCode::Error);
+
+   const auto matrix_4x4_1mult2 = SMT::Multiply(*matrix_4x4_1, *matrix_4x4_2);
+   EXPECT_EQ(matrix_4x4_1mult2.Code_, SMT::OperationResultCode::Ok);
+   auto func1mult2 = [func1, func2](size_t row, size_t column)->double 
+   { 
+      double result = 0.0;
+      for (size_t i = 0; i < 4; ++i)
+      {
+         result += func1(row, i) * func2(i, column);
+      }
+      return result;
+   };
+   CheckForEachElement<double>(*matrix_4x4_1mult2.Matrix_, func1mult2, false, 1.0);
+}
